e45c189700be480c4cfec11b1dec1d77564c74ec
sebastiankut <sebastian_kut@yahoo.co.uk>
2020-12-27 18:50:24 +0100

    added token grab and spotify API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { React, useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import Player from './Player';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  const getToken = () => {
+    const _token = getTokenFromUrl().access_token;
+    window.location.hash = '';
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log('user', user);
+      });
+    }
+  };
+
+  useEffect(() => {
+    getToken();
+  }, [token]);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
